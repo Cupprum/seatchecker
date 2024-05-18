bd25f3d189118c67ae0311a6b92aa0c3f1c83977
Cupprum <branisa.samuel@icloud.com>
2024-05-18 20:55:06 +0200

    test: account login

--- seatchecker_test.go
+++ seatchecker_test.go
@@ -1,68 +1,41 @@
-// package main
-
-// import (
-// 	"regexp"
-// 	"testing"
-// )
-
-// // TestHelloName calls greetings.Hello with a name, checking
-// // for a valid return value.
-// func TestHelloName(t *testing.T) {
-// 	name := "Gladys"
-// 	want := regexp.MustCompile(`\b` + name + `\b`)
-// 	msg, err := Hello("Gladys")
-// 	if !want.MatchString(msg) || err != nil {
-// 		t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
-// 	}
-// }
-
-// // TestHelloEmpty calls greetings.Hello with an empty string,
-// // checking for an error.
-//
-//	func TestHelloEmpty(t *testing.T) {
-//		msg, err := Hello("")
-//		if msg != "" || err == nil {
-//			t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
-//		}
-//	}
 package main
 
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestAccountLogin(t *testing.T) {
-
-	type CAuth struct {
-		CustomerID string `json:"customerId"`
-		Token      string `json:"token"`
-	}
-
-	b := CAuth{
-		"abc123",
-		"super_secret_token",
-	}
-	res, _ := json.Marshal(b)
+	e, p := "john@doe.com", "password"
+	cAReq := CAuth{"customerid", "token"}
 
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// fmt.Fprintln(w, res)
-		enc := json.NewEncoder(w)
-		enc.Encode(res)
+		rawB, _ := io.ReadAll(r.Body)
+		b := map[string]any{}
+		json.Unmarshal(rawB, &b)
+		if e != b["email"] {
+			t.Fatalf("wrong email, expected: %v, received: %v", e, b["email"])
+		}
+		if p != b["password"] {
+			t.Fatalf("wrong password, expected: %v, received: %v", e, b["password"])
+		}
+		res, _ := json.Marshal(cAReq)
+		fmt.Fprintln(w, string(res))
 	}))
 	defer ts.Close()
 
-	client := RClient{schema: "http", fqdn: ts.URL}
+	c := RClient{schema: "http", fqdn: ts.URL}
 
-	cAuth, err := client.accountLogin("john@doe.com", "secret")
-	if err != nil { // TODO: check also cAuth
+	cARes, err := c.accountLogin(e, p)
+	if err != nil {
 		t.Fatalf("failed to get account login: %v", err)
 	}
 
-	fmt.Println("rafds")
-	fmt.Println(cAuth)
-	fmt.Println(err)
+	if cAReq != cARes {
+		t.Fatalf("wrong response, expected: %v, received %v", cAReq, cARes)
+	}
 }
