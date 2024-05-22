38ae19e6175277269a43e096fbec4669e46289ef
Cupprum <branisa.samuel@icloud.com>
2024-05-22 18:29:36 +0200

    test: request creation and execution

--- seatchecker_test.go
+++ seatchecker_test.go
@@ -6,10 +6,94 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"reflect"
 	"strings"
 	"testing"
 )
+
+func TestRequestCreator(t *testing.T) {
+	q := url.Values{}
+	q.Add("query_parameter", "test_query_parameter")
+	h := http.Header{
+		"header": {"test_header"},
+	}
+	b := struct {
+		Payload string `json:"payload"`
+	}{
+		Payload: "test_payload",
+	}
+
+	r := Request{
+		method:      "GET",
+		scheme:      "http",
+		fqdn:        "test",
+		path:        "test_path",
+		queryParams: q,
+		headers:     h,
+		body:        b,
+	}
+
+	cr, err := r.creator()
+	if err != nil {
+		t.Fatalf("failed to create request: %v", err)
+	}
+
+	if r.method != cr.Method {
+		t.Fatalf("wrong method, expected: %v, received: %v", r.method, cr.Method)
+	}
+	if r.scheme != cr.URL.Scheme {
+		t.Fatalf("wrong scheme, expected: %v, received: %v", r.scheme, cr.URL.Scheme)
+	}
+	if r.fqdn != cr.URL.Host {
+		t.Fatalf("wrong url, expected: %v, received: %v", r.fqdn, cr.URL.Host)
+	}
+	if !strings.Contains(cr.URL.Path, r.path) {
+		t.Fatalf("wrong path, expected: %v, received: %v", r.path, cr.URL.Path)
+	}
+	if !reflect.DeepEqual(r.queryParams, cr.URL.Query()) {
+		t.Fatalf("wrong query parameters, expected: %v, received: %v", r.queryParams, cr.URL.Query())
+	}
+	if !reflect.DeepEqual(r.headers, cr.Header) {
+		t.Fatalf("wrong headers, expected: %v, received: %v", r.headers, cr.Header)
+	}
+
+	eb := "{\"payload\":\"test_payload\"}"
+	rrb, _ := io.ReadAll(cr.Body)
+	rb := string(rrb)
+	if eb != rb {
+		t.Fatalf("wrong body, expected: %v, received: %v", eb, rb)
+	}
+}
+
+func TestHttpsRequest(t *testing.T) {
+	ca := CAuth{
+		CustomerID: "test_customer_id",
+		Token:      "test_token",
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		res, _ := json.Marshal(ca)
+		fmt.Fprintln(w, string(res))
+	}))
+	defer ts.Close()
+
+	r := Request{
+		"POST",
+		"http",
+		ts.URL,
+		"test_path",
+		nil,
+		nil,
+		nil,
+	}
+	rca, _ := httpsRequest[CAuth](r)
+	fmt.Println(rca)
+
+	if !reflect.DeepEqual(ca, rca) {
+		t.Fatalf("returned struct is incorrect, expected: %v, received: %v", ca, rca)
+	}
+}
 
 func TestAccountLogin(t *testing.T) {
 	e, p := "john@doe.com", "password"
@@ -30,7 +114,7 @@ func TestAccountLogin(t *testing.T) {
 	}))
 	defer ts.Close()
 
-	c := RClient{schema: "http", fqdn: ts.URL}
+	c := RClient{scheme: "http", fqdn: ts.URL}
 
 	cARes, err := c.accountLogin(e, p)
 	if err != nil {
@@ -78,7 +162,7 @@ func TestGetBookingId(t *testing.T) {
 	defer ts.Close()
 
 	// Check received response
-	c := RClient{schema: "http", fqdn: ts.URL}
+	c := RClient{scheme: "http", fqdn: ts.URL}
 	rId, err := c.getBookingId(cAReq)
 	if err != nil {
 		t.Fatalf("failed to get booking id: %v", err)
@@ -110,7 +194,7 @@ func TestGetBookingById(t *testing.T) {
 	defer ts.Close()
 
 	// Check received response
-	c := RClient{schema: "http", fqdn: ts.URL}
+	c := RClient{scheme: "http", fqdn: ts.URL}
 	cAReq := CAuth{"customerid", "token"}
 	bA, err := c.getBookingById(cAReq, "booking_id")
 	if err != nil {
@@ -148,7 +232,7 @@ func TestCreateBasket(t *testing.T) {
 	defer ts.Close()
 
 	// Check received response
-	c := RClient{schema: "http", fqdn: ts.URL}
+	c := RClient{scheme: "http", fqdn: ts.URL}
 
 	bId, err := c.createBasket(bA)
 	if err != nil {
@@ -184,7 +268,7 @@ func TestGetSeatsQuery(t *testing.T) {
 	defer ts.Close()
 
 	// Check received response
-	c := RClient{schema: "http", fqdn: ts.URL}
+	c := RClient{scheme: "http", fqdn: ts.URL}
 
 	qs, err := c.getSeatsQuery(bId)
 	if err != nil {
