a28f8d672069b1234116c984bf36561c917783b7
Cupprum <branisa.samuel@icloud.com>
2024-05-24 00:47:04 +0200

    feat: terraform deploy basic lambda

--- infra/main.tf
+++ infra/main.tf
@@ -12,27 +12,39 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-data "archive_file" "lambda" {
-  type        = "zip"
-  source_file = "lambda.js"
-  output_path = "lambda_function_payload.zip"
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
 resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
+  filename      = "seatChecker.zip"
+  function_name = "seat_checker"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.test"
+  handler       = "bootstrap"
+  architectures = [ "arm64" ]
 
-  source_code_hash = data.archive_file.lambda.output_base64sha256
-
-  runtime = "nodejs18.x"
+  runtime = "provided.al2023"
 
   environment {
     variables = {
-      foo = "bar"
+      SEATCHECKER_EMAIL    = "xxx"
+      SEATCHECKER_PASSWORD = "xxx"
     }
   }
 }
