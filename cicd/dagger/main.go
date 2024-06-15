610eab9160e109cd2a7f70ee58ad7cf2b02399d4
Cupprum <branisa.samuel@icloud.com>
2024-06-15 19:03:16 +0200

    ci: refactor dagger pipeline, remove the need to have alpine container

--- cicd/dagger/main.go
+++ cicd/dagger/main.go
@@ -6,37 +6,36 @@ import (
 
 type Cicd struct{}
 
-func (m *Cicd) Build(src *Directory) *Container {
-	return dag.Container().
+func Logic(src *Directory) *Directory {
+	base := dag.Container().
 		From("golang:latest").
+		WithExec([]string{"apt", "update"}).
+		WithExec([]string{"apt", "install", "zip", "-y"}). // Required to package lambda.
 		WithEnvVariable("tes", "4").
 		WithDirectory("/src", src).
 		WithWorkdir("/src").
 		WithEnvVariable("GOOS", "linux").
 		WithEnvVariable("GOARCH", "arm64").
-		WithExec([]string{"go", "install"}).
-		WithExec([]string{"go", "build",
-			"-tags", "lambda.norpc", // Do not include RPC part of library.
-			"-o", "seatchecker",
-			"-ldflags", "-w", // Reduce size of output binary.
-			"seatchecker.go"}).
+		WithExec([]string{"go", "install"})
+
+	base.WithExec([]string{"go", "test"})
+
+	build := base.WithExec([]string{"go", "build",
+		"-tags", "lambda.norpc", // Do not include RPC part of library.
+		"-o", "seatchecker",
+		"-ldflags", "-w", // Reduce size of output binary.
+		"seatchecker.go"})
+
+	pack := build.
 		WithExec([]string{"mkdir", "/out"}).
-		WithExec([]string{"mv", "seatchecker", "/out/seatchecker"})
-}
-
-func (m *Cicd) Package(out *Directory) *Container {
-	return dag.Container().
-		From("alpine:latest").
-		WithEnvVariable("tes", "4").
-		WithExec([]string{"apk", "add", "zip"}).
-		WithDirectory("/out", out).
+		WithExec([]string{"mv", "seatchecker", "/out/bootstrap"}). // Lambda requires it to be called bootstrap.
 		WithWorkdir("/out").
-		WithExec([]string{"mv", "seatchecker", "bootstrap"}). // Lambda requires it to be called bootstrap.
-		WithExec([]string{"zip", "seatchecker.zip", "bootstrap"}).
-		WithExec([]string{"rm", "bootstrap"}) // Cleanup so the transfered directory is smaller.
+		WithExec([]string{"zip", "seatchecker.zip", "bootstrap"})
+
+	return pack.Directory("/out")
 }
 
-func (m *Cicd) Deploy(out *Directory, infra *Directory,
+func Infra(out *Directory, infra *Directory,
 	ak *Secret, sk *Secret,
 ) *Container {
 	return dag.Container().
@@ -56,9 +55,8 @@ func (m *Cicd) Run(ctx context.Context,
 	logic *Directory, infra *Directory,
 	access_key *Secret, secret_key *Secret,
 ) (string, error) {
-	build := m.Build(logic)
-	pack := m.Package(build.Directory("/out"))
-	deploy := m.Deploy(pack.Directory("/out"), infra, access_key, secret_key)
+	out := Logic(logic)
+	i := Infra(out, infra, access_key, secret_key)
 
-	return deploy.Stdout(ctx)
+	return i.Stdout(ctx)
 }
