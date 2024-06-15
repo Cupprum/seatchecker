ec23272522d88f52e9a3f43034d3cff47cc8bd77
Cupprum <branisa.samuel@icloud.com>
2024-06-15 20:58:17 +0200

    ci: dagger pipeline is refactored and supports go and terraform caching

--- cicd/dagger/main.go
+++ cicd/dagger/main.go
@@ -9,20 +9,26 @@ import (
 type Cicd struct{}
 
 func Logic(src *Directory) *Directory {
-	base := dag.Container().
-		From("golang:latest").
-		WithEnvVariable("CACHE", "2").
+	base := dag.Container().From("golang:latest")
+	// Install packages required to package Lambda.
+	base = base.
 		WithExec([]string{"apt", "update"}).
-		WithExec([]string{"apt", "install", "zip", "-y"}). // Required to package lambda.
+		WithExec([]string{"apt", "install", "zip", "-y"})
+	// Configure source directory.
+	base = base.
 		WithDirectory("/src", src).
-		WithWorkdir("/src").
+		WithWorkdir("/src")
+	// Configure Lambda runtime.
+	base = base.
 		WithEnvVariable("GOOS", "linux").
-		WithEnvVariable("GOARCH", "arm64").
+		WithEnvVariable("GOARCH", "arm64")
+	// Add cache for Go.
+	base = base.
 		WithMountedCache("/go/pkg/mod", dag.CacheVolume("go-mod-logic")).
 		WithEnvVariable("GOMODCACHE", "/go/pkg/mod").
 		WithMountedCache("/go/build-cache", dag.CacheVolume("go-build-logic")).
-		WithEnvVariable("GOCACHE", "/go/build-cache").
-		WithExec([]string{"go", "install"})
+		WithEnvVariable("GOCACHE", "/go/build-cache")
+	base = base.WithExec([]string{"go", "install"})
 
 	base.WithExec([]string{"go", "test"})
 
@@ -32,35 +38,40 @@ func Logic(src *Directory) *Directory {
 		"-ldflags", "-w", // Reduce size of output binary.
 		"seatchecker.go"})
 
-	pack := build.
+	out := build.
 		WithExec([]string{"mkdir", "/out"}).
 		WithExec([]string{"mv", "seatchecker", "/out/bootstrap"}). // Lambda requires it to be called bootstrap.
 		WithWorkdir("/out").
 		WithExec([]string{"zip", "seatchecker.zip", "bootstrap"})
 
-	return pack.Directory("/out")
+	return out.Directory("/out")
 }
 
 func Infra(out *Directory, infra *Directory,
 	ak *Secret, sk *Secret,
 ) *Container {
-	base := dag.Container().
-		From("hashicorp/terraform:latest").
-		WithEnvVariable("CACHE", "2").
+	base := dag.Container().From("hashicorp/terraform:latest")
+	// Configure source directories.
+	base = base.
 		WithDirectory("/out", out).
 		WithDirectory("/infra", infra).
-		WithWorkdir("/infra").
+		WithWorkdir("/infra")
+	// Configure AWS credentials.
+	base = base.
 		WithSecretVariable("AWS_ACCESS_KEY_ID", ak).
 		WithSecretVariable("AWS_SECRET_ACCESS_KEY", sk).
 		WithEnvVariable("AWS_REGION", "eu-central-1")
+	// Configure cache for Terraform plugins.
+	base = base.
+		WithMountedCache("/infra/terraform.d/plugins", dag.CacheVolume("terraform"))
 
 	tf := base.WithExec([]string{"init"})
 
-	// Ensure terraform operation is never cached.
+	// Ensure that Terraform apply operation is never cached.
 	epoch := fmt.Sprintf("%d", time.Now().Unix())
-	tf = tf.WithEnvVariable("CACHEBUSTER", epoch)
-
-	tf = tf.WithExec([]string{"apply", "-auto-approve"})
+	tf = tf.
+		WithEnvVariable("CACHEBUSTER", epoch).
+		WithExec([]string{"apply", "-auto-approve"})
 
 	return tf
 }
