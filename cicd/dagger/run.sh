1b00d37b194d7de3ac9dbf73c59686e35db1d48e
Cupprum <branisa.samuel@icloud.com>
2024-06-16 21:11:27 +0200

    chore: restructure logic

--- cicd/dagger/run.sh
+++ cicd/dagger/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 dagger call run \
-    --logic="../../logic" \
+    --logic="../../logic/seatchecker" \
     --infra="../../infra" \
     --access_key="env:GREENMO_AWS_ACCESS_KEY_ID" \
     --secret_key="env:GREENMO_AWS_SECRET_ACCESS_KEY"
